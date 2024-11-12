60281fb274dc272a1d149be9b8fecc56b82a0de4
naut74 <ngoctuan14032001@gmail.com>
2024-11-12 10:25:03 +0700

    demo

---
+++ demo/src/InputValueToArray.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class InputValueToArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[][] matrix = new int[4][4];
+        System.out.println("Enter " + matrix.length + " rows and " +
+                matrix[0].length + " columns: ");
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = input.nextInt();
+            }
+        }
+//        System.out.println(matrix[1][3]);
+        // sum array
+        int total = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                total += matrix[row][column];
+            }
+        }
+        // sum column
+        for (int column = 0; column < matrix[0].length; column++) {
+            int total1 = 0;
+            for (int row = 0; row < matrix.length; row++)
+                total += matrix[row][column];
+            System.out.println("Sum for column " + column + " is "
+                    + total);
+        }
+        // find row have max total
+        int maxRow = 0;
+        int indexOfMaxRow = 0;
+            // Get sum of the first row in maxRow
+        for (int column = 0; column < matrix[0].length; column++) {
+            maxRow += matrix[0][column];
+        }
+        for (int row = 1; row < matrix.length; row++) {
+            int totalOfThisRow = 0;
+            for (int column = 0; column < matrix[row].length; column++)
+                totalOfThisRow += matrix[row][column];
+            if (totalOfThisRow > maxRow) {
+                maxRow = totalOfThisRow;
+                indexOfMaxRow = row;
+            }
+        }
+        System.out.println("Row " + indexOfMaxRow + " has the maximum sum of " + maxRow);
+        // Array Shuffle
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int i1 = (int) (Math.random() * matrix.length);
+                int j1 = (int) (Math.random() * matrix[i].length);
+
+                // Swap matrix[i][j] with matrix[i1][j1]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i1][j1];
+                matrix[i1][j1] = temp;
+            }
+        }
+    }
+}
