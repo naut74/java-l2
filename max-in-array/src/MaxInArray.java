f3b5ca926125ba9fb2f70455b6b7d07e8279f2a5
naut74 <ngoctuan14032001@gmail.com>
2024-11-12 21:51:20 +0700

    max in array

---
+++ max-in-array/src/MaxInArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class MaxInArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Enter number of cols: ");
+        int cols = input.nextInt();
+
+        int[][] array = new int[rows][cols];
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Element [" + i + "][" + j + "]: ");
+                array[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("Array is: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        int max = array[0][0];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (max < array[i][j]) {
+                    max = array[i][j];
+                }
+            }
+        }
+        System.out.printf("\nMax in array is: " + max);
+    }
+}
